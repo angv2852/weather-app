6849b8338a727071f0028df8681315485433c740
Ang <angv2852@gmail.com>
2021-12-10 12:48:01 -0700

    Integrated API to show live forecast data

--- script.js
+++ script.js
@@ -21,22 +21,38 @@ if (minutes < 10) {
 }
 p.innerHTML = `Last updated ${day}, ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
                 <div class="forecast-day">
-                <strong>${day}</strong> </div>
-                <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="60">
+                <strong>${formatDay(forecastDay.dt)}</strong> </div>
+                <img src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" alt="" width="60">
             <span class="forecast-temp-max">
-                <strong>55°</strong></span><span class="forecast-temp-min">/ 30°</span>
+                <strong>${Math.round(
+                  forecastDay.temp.max
+                )}°</strong></span><span class="forecast-temp-min">/ ${Math.round(
+          forecastDay.temp.min
+        )}°</span>
                 
             </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
